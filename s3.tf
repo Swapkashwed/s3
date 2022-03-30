2795041f15888bc103c7feef028f3f8a6980ec46
Swapkashwed <102313176+Swapkashwed@users.noreply.github.com>
2022-03-30 20:40:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,9 @@
+# as per requrement
 resource "aws_s3_bucket" "swapsksk010" {
   bucket = "swapskmybucket"
 
   tags = {
     Name        = "My Bucket"
-    Environment = "Dev"
+    Environment = "prod"
   }
 }
